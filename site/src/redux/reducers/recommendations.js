d6b2f27a9b60f35ce9b00190cafcf99817ca3eee
Wei Gao <wgao19@gmail.com>
2019-05-31 01:23:32 +0800

    Automatically generate recommendation list

--- site/src/redux/reducers/recommendations.js
+++ site/src/redux/reducers/recommendations.js
@@ -1,10 +1,23 @@
-const initState = {
-  1: [1],
-  2: [1, 2]
-};
+import { PUSH } from "../actionTypes";
+const initState = {};
 
 const recommendations = (state = initState, action) => {
-  return state;
+  switch (action.type) {
+    case PUSH: {
+      const { id } = action.payload;
+      const list = [];
+      for (let i = 1; i < id; i++) {
+        list.push(i);
+      }
+      return {
+        ...state,
+        [id]: list
+      };
+    }
+    default: {
+      return state;
+    }
+  }
 };
 
 export default recommendations;
